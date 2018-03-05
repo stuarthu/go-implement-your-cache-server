5cc44a0f076568f12d8864a58f13071523a98678
Stuart <Stuart.Hu@emc.com>
2018-03-06 03:05:17 +0800

    add chapter1/test
    Signed-off-by: Stuart <Stuart.Hu@emc.com>

---
+++ chapter1/test/test.sh
@@ -0,0 +1,17 @@
+curl 127.0.0.1:12345/status
+
+curl -v 127.0.0.1:12345/cache/testkey -XPUT -dtestvalue
+
+curl 127.0.0.1:12345/cache/testkey
+
+curl 127.0.0.1:12345/status
+
+curl 127.0.0.1:12345/cache/testkey -XDELETE
+
+curl 127.0.0.1:12345/status
+
+./cache-benchmark -type http -n 100000 -t set
+
+./cache-benchmark -type http -n 100000 -t get
+
+redis-benchmark -c 1 -n 100000 -d 1000 -t set,get
