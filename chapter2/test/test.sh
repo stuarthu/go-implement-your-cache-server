544678f470cd73ae33d56fd6d409ae1a79bfc4fb
Stuart <Stuart.Hu@emc.com>
2018-03-07 02:34:43 +0800

    update code
    Signed-off-by: Stuart <Stuart.Hu@emc.com>

---
+++ chapter2/test/test.sh
@@ -0,0 +1,13 @@
+../client/client -c set -k testkey -v testvalue
+
+../client/client -c get -k testkey
+
+curl 127.0.0.1:12345/status
+
+../client/client -c del -k testkey
+
+curl 127.0.0.1:12345/status
+
+../cache-benchmark/cache-benchmark -type tcp -n 100000 -t set
+
+../cache-benchmark/cache-benchmark -type tcp -n 100000 -t get
