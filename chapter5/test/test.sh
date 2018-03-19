a2379c903c3190f032f98143aca882afcf084e8d
Stuart <Stuart.Hu@emc.com>
2018-03-20 02:41:58 +0800

    update test
    Signed-off-by: Stuart <Stuart.Hu@emc.com>

--- chapter5/test/test.sh
+++ chapter5/test/test.sh
@@ -1,3 +1,7 @@
 ./cache-benchmark -type tcp -n 100000 -r 100000 -t set
 
 ./cache-benchmark -type tcp -n 100000 -r 100000 -t set -P 3
+
+./cache-benchmark -type tcp -n 100000 -r 100000 -t set -c 50
+
+redis-benchmark -c 50 -n 100000 -d 1000 -t set -r 100000 -P 1
